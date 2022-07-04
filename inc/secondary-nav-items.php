8433701e7ff467e6102240843154b7199fb08129
Goce Mitevski <goce.mitevski@keitaro.com>
2022-07-04 08:16:54 +0200

    Refactor secondary-nav-item for wpcs

--- inc/secondary-nav-items.php
+++ inc/secondary-nav-items.php
@@ -1,42 +1,47 @@
 <?php
+/**
+ * Template for rendering secondary navigation items accross the theme
+ *
+ * @package OKFNWP
+ */
 
-$theme_options = get_option( 'theme_options_option_name' );
+$okf_theme_options = get_option( 'theme_options_option_name' );
 
-if ( ! empty( $theme_options['okfnwp_fb_id'] ) ) {
+if ( ! empty( $okf_theme_options['okfnwp_fb_id'] ) ) {
 	?>
 <li class="nav-item">
-  <a class="facebook nav-link" href="https://www.facebook.com/<?php echo isset( $theme_options['okfnwp_fb_id'] ) ? esc_attr( $theme_options['okfnwp_fb_id'] ) : 'OKFNetwork'; ?>">Facebook</a>
+  <a class="facebook nav-link" href="https://www.facebook.com/<?php echo isset( $okf_theme_options['okfnwp_fb_id'] ) ? esc_attr( $okf_theme_options['okfnwp_fb_id'] ) : 'OKFNetwork'; ?>">Facebook</a>
 </li>
 	<?php
 }
-if ( ! empty( $theme_options['okfnwp_twitter_id'] ) ) {
+if ( ! empty( $okf_theme_options['okfnwp_twitter_id'] ) ) {
 	?>
 <li class="nav-item">
-  <a class="twitter nav-link" href="https://twitter.com/<?php echo isset( $theme_options['okfnwp_twitter_id'] ) ? esc_attr( $theme_options['okfnwp_twitter_id'] ) : 'okfn'; ?>">Twitter</a>
+  <a class="twitter nav-link" href="https://twitter.com/<?php echo isset( $okf_theme_options['okfnwp_twitter_id'] ) ? esc_attr( $okf_theme_options['okfnwp_twitter_id'] ) : 'okfn'; ?>">Twitter</a>
 </li>
 	<?php
 }
-if ( ! empty( $theme_options['okfnwp_discuss_id'] ) ) {
+if ( ! empty( $okf_theme_options['okfnwp_discuss_id'] ) ) {
 	?>
 <li class="nav-item">
-  <a class="discuss nav-link" href="https://discuss.okfn.org/<?php echo isset( $theme_options['okfnwp_discuss_id'] ) ? esc_attr( $theme_options['okfnwp_discuss_id'] ) : ''; ?>"><?php _e( 'Forum', 'okfnwp' ); ?></a>
+  <a class="discuss nav-link" href="https://discuss.okfn.org/<?php echo isset( $okf_theme_options['okfnwp_discuss_id'] ) ? esc_attr( $okf_theme_options['okfnwp_discuss_id'], 'okfnwp' ) : ''; ?>"><?php esc_html_e( 'Forum', 'okfnwp' ); ?></a>
 </li>
 	<?php
 }
-if ( ! empty( $theme_options['okfnwp_donate_url'] ) ) {
+if ( ! empty( $okf_theme_options['okfnwp_donate_url'] ) ) {
 	?>
 <li class="nav-item">
-  <a class="nav-link" href="<?php echo $theme_options['okfnwp_donate_url']; ?>">
-	<?php _e( 'Donate', 'okfnwp' ); ?>
+  <a class="nav-link" href="<?php esc_url_raw( $okf_theme_options['okfnwp_donate_url'], 'okfnwp' ); ?>">
+	<?php esc_html_e( 'Donate', 'okfnwp' ); ?>
   </a>
 </li>
 	<?php
 }
-if ( ! empty( $theme_options['okfnwp_newsletter_url'] ) ) {
+if ( ! empty( $okf_theme_options['okfnwp_newsletter_url'] ) ) {
 	?>
 <li class="nav-item">
-  <a class="nav-link" href="<?php echo $theme_options['okfnwp_newsletter_url']; ?>">
-	<?php _e( 'Subscribe', 'okfnwp' ); ?>
+  <a class="nav-link" href="<?php esc_url_raw( $okf_theme_options['okfnwp_newsletter_url'], 'okfnwp' ); ?>">
+	<?php esc_html_e( 'Subscribe', 'okfnwp' ); ?>
   </a>
 </li>
 	<?php
