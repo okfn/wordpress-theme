f3155d4e3045862346cc69351fc011f34ecde3fe
Goce Mitevski <goce.mitevski@keitaro.com>
2022-07-04 16:05:19 +0200

    Fix variable name errors

--- author-info.php
+++ author-info.php
@@ -9,37 +9,37 @@ if ( function_exists( 'coauthors' ) ) :
 	get_template_part( 'author', 'info-coauthors' );
 else :
 
-	$author             = get_the_author();
-	$author_description = get_the_author_meta( 'description' );
-	$avatar             = get_avatar( get_the_author_meta( 'ID' ), 80 );
+	$okf_author             = get_the_author();
+	$okf_author_description = get_the_author_meta( 'description' );
+	$okf_avatar             = get_avatar( get_the_author_meta( 'ID' ), 80 );
 
 	/*
 	 Prepare a suitable URL for the Gravatar user profile, as recommended here:
 	 *  https://en.gravatar.com/site/implement/profiles/
 	 */
 
-	$avatar_url = 'https://www.gravatar.com/' . md5( get_the_author_meta( 'user_email' ) );
+	$okf_avatar_url = 'https://www.gravatar.com/' . md5( get_the_author_meta( 'user_email' ) );
 
-	if ( empty( $author_description ) ) :
-		$author_description = __( 'No description set for this author.', 'okfnwp' );
+	if ( empty( $okf_author_description ) ) :
+		$okf_author_description = __( 'No description set for this author.', 'okfnwp' );
 	endif;
 
-	if ( $author ) :
+	if ( $okf_author ) :
 
 		?>
 		<aside class="author-info">
-			<a class="author-info_link" href="<?php echo esc_url( $avatar_url ); ?>">
-				<span class="author-info_thumbnail"><?php echo wp_kses_post( $avatar ); ?></span>
+			<a class="author-info_link" href="<?php echo esc_url( $okf_avatar_url ); ?>">
+				<span class="author-info_thumbnail"><?php echo wp_kses_post( $okf_avatar ); ?></span>
 			</a>
 			<h4 class="author-info_name">
 			<?php
 
 				// translators: %1$s stands for the author name.
-				echo esc_html( sprintf( __( 'About %1$s' ), get_the_author_meta( 'display_name' ) ) );
+				echo esc_html( sprintf( __( 'About %1$s', 'okfnwp' ), get_the_author_meta( 'display_name' ) ) );
 
 			?>
 				</h4>
-			<p><?php echo wp_kses_post( $author_description ); ?></p>
+			<p><?php echo wp_kses_post( $okf_author_description ); ?></p>
 		</aside>
 		<?php
 
