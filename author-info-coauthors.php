bafd74cb680c906b6f7e6688b39da0fcfffc8dd1
Goce Mitevski <goce.mitevski@keitaro.com>
2022-07-05 12:12:44 +0200

    Fix variable name errors

--- author-info-coauthors.php
+++ author-info-coauthors.php
@@ -4,43 +4,44 @@
  *
  * @package OKFNWP
  */
+
 if ( function_exists( 'get_coauthors' ) ) :
-	$authors = get_coauthors( get_the_ID() );
+	$okf_authors = get_coauthors( get_the_ID() );
 endif;
 
-if ( is_array( $authors ) ) :
+if ( is_array( $okf_authors ) ) :
 
-	foreach ( $authors as $author ) :
+	foreach ( $okf_authors as $okf_author ) :
 
-		$author_description = get_the_author_meta( 'description', $author->ID );
-		$avatar             = get_avatar( get_the_author_meta( 'ID' ), 80 );
+		$okf_author_description = get_the_author_meta( 'description', $okf_author->ID );
+		$okf_avatar             = get_avatar( get_the_author_meta( 'ID' ), 80 );
 
 		/*
 		 Prepare a suitable URL for the Gravatar user profile, as recommended here:
 		 *  https://en.gravatar.com/site/implement/profiles/
 		 */
-		$avatar_url = 'https://www.gravatar.com/' . md5( get_the_author_meta( 'user_email', $author->ID ) );
+		$okf_avatar_url = 'https://www.gravatar.com/' . md5( get_the_author_meta( 'user_email', $okf_author->ID ) );
 
-		if ( empty( $author_description ) ) :
-			$author_description = __( 'No description set for this author.', 'okfnwp' );
+		if ( empty( $okf_author_description ) ) :
+			$okf_author_description = __( 'No description set for this author.', 'okfnwp' );
 		endif;
 
-		if ( $author ) :
+		if ( $okf_author ) :
 
 			?>
 			<aside class="author-info">
-				<a class="author-info_link" href="<?php echo esc_url( $avatar_url ); ?>">
-					<span class="author-info_thumbnail"><?php echo wp_kses_post( $avatar ); ?></span>
+				<a class="author-info_link" href="<?php echo esc_url( $okf_avatar_url ); ?>">
+					<span class="author-info_thumbnail"><?php echo wp_kses_post( $okf_avatar ); ?></span>
 				</a>
 				<h4 class="author-info_name">
 				<?php
 
-					// translators: %1$s stands for the author name
-					echo esc_html( sprintf( __( 'About %1$s' ), get_the_author_meta( 'display_name', $author->ID ) ) );
+					// translators: %1$s stands for the author name.
+					echo esc_html( sprintf( __( 'About %1$s', 'okfnwp' ), get_the_author_meta( 'display_name', $okf_author->ID ) ) );
 
 				?>
 					</h4>
-				<p><?php echo wp_kses_post( $author_description ); ?></p>
+				<p><?php echo wp_kses_post( $okf_author_description ); ?></p>
 			</aside>
 			<?php
 
--- content-post-thumb.php
+++ content-post-thumb.php
@@ -1,25 +1,27 @@
 <?php
 /**
+ * The post thumbnail template
+ *
  * @package OKFNWP
  */
 
 $okf_categories = get_the_category();
 
 if ( has_post_thumbnail() ) :
-	$thumb = get_the_post_thumbnail( $post, 'small' );
+	$okf_thumb = get_the_post_thumbnail( $post, 'small' );
 elseif ( okfn_get_first_image_url_from_post_content() ) :
-	$thumb = sprintf( '<img class="attachment-small size-small wp-post-image" src="%s" alt="">', okfn_get_first_image_url_from_post_content() );
+	$okf_thumb = sprintf( '<img class="attachment-small size-small wp-post-image" src="%s" alt="">', okfn_get_first_image_url_from_post_content() );
 else :
-	$thumb = null;
+	$okf_thumb = null;
 endif;
 
-if ( $thumb ) :
+if ( $okf_thumb ) :
 
 	?>
   <div class="post__thumb">
 	<?php
 
-	echo '<a class="post__thumb-link" href="' . esc_url( get_permalink() ) . '">' . wp_kses_post( $thumb ) . '</a>';
+	echo '<a class="post__thumb-link" href="' . esc_url( get_permalink() ) . '">' . wp_kses_post( $okf_thumb ) . '</a>';
 
 	if ( $okf_categories ) :
 		echo wp_kses_post( sprintf( '<a href="%1$s" class="post__category">%2$s</a>', get_category_link( $okf_categories[0]->term_id ), $okf_categories[0]->name ) );
